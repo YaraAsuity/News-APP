657a0e132bcc4150e869a63cace859bdf59d7a84
Yara Asuity <120588069+YaraAsuity@users.noreply.github.com>
2024-06-17 16:06:07 +0300

    Update NewsResponse.kt

--- app/src/main/java/com/yara/thenewsapp/models/NewsResponse.kt
+++ app/src/main/java/com/yara/thenewsapp/models/NewsResponse.kt
@@ -3,7 +3,7 @@ package com.yara.thenewsapp.models
 import com.yara.thenewsapp.models.Article
 
 data class NewsResponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )
