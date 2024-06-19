0fbfb366ef8dff948e130d11771ddda1bf9629dc
Yara Asuity <120588069+YaraAsuity@users.noreply.github.com>
2024-06-19 22:18:53 +0300

    Update HeadlinesFragment.kt

--- app/src/main/java/com/yara/thenewsapp/ui/HeadlinesFragment.kt
+++ app/src/main/java/com/yara/thenewsapp/ui/HeadlinesFragment.kt
@@ -1,60 +1,78 @@
-package com.yara.thenewsapp.ui
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yara.thenewsapp.R
+import com.yara.thenewsapp.adapters.NewsAdapter
+import com.yara.thenewsapp.databinding.FragmentHeadlinesBinding
+import com.yara.thenewsapp.ui.NewsViewModel
+import com.yara.thenewsapp.uitl.Resource
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HeadlinesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HeadlinesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var newsViewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var retryButton: Button
+    private lateinit var errorText: TextView
+    private lateinit var itemHeadLinesError: CardView
+    private lateinit var binding: FragmentHeadlinesBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var isError = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var isScrolling = false
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHeadlinesBinding.bind(view)
+
+        newsViewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
+
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun hideErrorMessage() {
+        itemHeadLinesError.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private fun showErrorMessage(message: String) {
+        itemHeadLinesError.visibility = View.VISIBLE
+        errorText.text = message
+        isError = true
+    }
+    val scrollListener=object :RecyclerView.OnScrollListener(){
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager=recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition=layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount=layoutManager.childCount
+            val totalItemCount=layoutManager.itemCount
+            val isNoError=!isError
+            val isNotLoadingAndNotLastPage=!isLoading&&!isLastPage
+            val isAtLastItem=firstVisibleItemPosition+visibleItemCount>=totalItemCount
+            val isNotAtBeginning =firstVisibleItemPosition>=0
+            
+
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HeadlinesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HeadlinesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
